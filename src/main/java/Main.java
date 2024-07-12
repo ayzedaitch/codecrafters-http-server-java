6763477d813dc83fd1f21d378f61a9e219312956
AyZedAitch <adam.houlihal@usmba.ac.ma>
2024-07-12 14:11:54 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,14 +25,22 @@ public class Main {
          String response = "HTTP/1.1 404 Not Found\r\n\r\n";
          if(requestParts[1].equals("/")) {
              response = "HTTP/1.1 200 OK\r\n\r\n";
-             clientSocket.getOutputStream().write(response.getBytes());
          } else if (requestParts[1].startsWith("/echo")) {
              String str = requestParts[1].substring(6);
              response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str.length() + "\r\n\r\n" + str;
-             clientSocket.getOutputStream().write(response.getBytes());
-         } else {
-             clientSocket.getOutputStream().write(response.getBytes());
+         } else if (requestParts[1].equals("/user-agent")) {
+             StringBuilder headers = new StringBuilder();
+             String line = reader.readLine();
+             String userAgent = "";
+             while (line != null && !line.isEmpty()) {
+                 if (line.startsWith("User-Agent")) {
+                     userAgent = line.substring(line.indexOf(":") + 2);
+                 }
+                 line = reader.readLine();
+             }
+             response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length() + "\r\n\r\n" + userAgent;
          }
+         clientSocket.getOutputStream().write(response.getBytes());
        System.out.println("accepted new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
