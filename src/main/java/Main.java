6fbe8537c732ace1a043ac44e077793023f49847
AyZedAitch <adam.houlihal@usmba.ac.ma>
2024-07-14 15:31:41 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,6 @@ public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     //
      ServerSocket serverSocket = null;
@@ -21,7 +20,7 @@ public class Main {
          serverSocket.setReuseAddress(true);
          while (true) {
              clientSocket = serverSocket.accept(); // Wait for connection from client.
-            new Thread(new RequestHandler(clientSocket)).start();
+            new Thread(new RequestHandler(clientSocket, args)).start();
          }
      } catch (IOException e){
          System.out.println("IOException: " + e.getMessage());
--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -1,39 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RequestHandler implements Runnable{
 
     private final Socket clientSocket;
+    private final String[] args;
 
-    public RequestHandler(Socket clientSocket) {
+    public RequestHandler(Socket clientSocket, String[] args) {
         this.clientSocket = clientSocket;
+        this.args = args;
     }
 
     @Override
     public void run() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String[] requestParts = reader.readLine().split(" ");
+            String[] requestParts = reader.readLine().split(" "); // Read the first line of the request
             String response = "HTTP/1.1 404 Not Found\r\n\r\n";
             if(requestParts[1].equals("/")) {
                 response = "HTTP/1.1 200 OK\r\n\r\n";
-            } else if (requestParts[1].startsWith("/echo")) {
-                String str = requestParts[1].substring(6);
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str.length() + "\r\n\r\n" + str;
+            } else if (requestParts[1].startsWith("/echo/")) {
+                String str = requestParts[1].substring(6); // /echo/{str}
+                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + str.getBytes().length + "\r\n\r\n" + str;
             } else if (requestParts[1].equals("/user-agent")) {
-                StringBuilder headers = new StringBuilder();
                 String line = reader.readLine();
                 String userAgent = "";
                 while (line != null && !line.isEmpty()) {
                     if (line.startsWith("User-Agent")) {
-                        userAgent = line.substring(line.indexOf(":") + 2);
+                        userAgent = line.substring(line.indexOf(":") + 2); // User-Agent: {value}
+                        break;
                     }
                     line = reader.readLine();
                 }
-                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.length() + "\r\n\r\n" + userAgent;
+                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent.getBytes().length + "\r\n\r\n" + userAgent;
+            } else if (requestParts[1].startsWith("/files/")) {
+                String path = null;
+                for (int i = 0; i < args.length; i++) {
+                    if (args[i].equals("--directory")) {
+                        path = args[++i] + requestParts[1].substring(7);;
+                        break;
+                    }
+                }
+                try{
+                    String content = Files.readString(Paths.get(path));
+                    response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + content.getBytes().length + "\r\n\r\n" + content;
+                } catch (Exception e) {
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                }
+
             }
             clientSocket.getOutputStream().write(response.getBytes());
             System.out.println("accepted new connection");
